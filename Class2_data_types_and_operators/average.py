8b782e5718455ba8434cd616e6a0ceabd681b2cc
Jose Luis <jlperezcaballero@gmail.com>
2021-06-02 12:19:02 +0200

    Second commit // testing github authentication

--- Class2_data_types_and_operators/average.py
+++ Class2_data_types_and_operators/average.py
@@ -3,14 +3,14 @@
 # Mode1: direct
 print ((23+32+64)/3)
 
-# Mode2: Numpy
-from Numpy import mean
-print (mean([23,32,64]))
-
-# Mode3: Statistics
-from statistics import mean
-print (mean([23,32,64]))
-
 # Mode4: Con operaciones de lista
 list = [23,32,64]
 print (sum(list)/len(list))
+
+# Mode3: Statistics (libreria preinstalada con python 3.9.1)
+from statistics import mean
+print (mean([23,32,64]))
+
+# Mode2: Numpy (librería que hay que instalar aparte)
+from Numpy import mean
+print (mean([23,32,64]))
